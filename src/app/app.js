7ab18fe167eb7b32fce3be414a1c5ec926e3a8b8
John Lenin Ortiz Gamba <mifirma@noproduccion.onmicrosoft.com>
2021-08-25 18:10:14 -0500

    remember me funct 5

--- src/app/app.js
+++ src/app/app.js
@@ -980,6 +980,6 @@ window.onload = function () {
     document.getElementById("login-form").onsubmit = onSubmit;
     setTimeout(function () {
       document.getElementById("submit-btn").click();
-    }, 500);
+    }, 2000);
   }
 };
