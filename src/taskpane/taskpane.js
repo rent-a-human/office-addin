c95cee977ed22405f10c7e4e0f1d20fde49f31ad
John Lenin Ortiz Gamba <mifirma@noproduccion.onmicrosoft.com>
2021-09-03 01:09:44 -0500

    added loader 1.4

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -181,13 +181,10 @@ export async function runExcel() {
 
 export async function runPowerPoint() {
   return PowerPoint.run(async (context) => {
+    document.getElementById("load-banner").style.display = "block";
     localStorage.setItem("outsideOffice", true);
     localStorage.setItem("userFromOffice", false);
     localStorage.setItem("noob", "0"); //garantiza que no usa info de office, ya que fue llamado desde clic en iniciar sesion
-    Office.context.ui.displayDialogAsync("https://nervous-aryabhata-655da7.netlify.app/app.html", {
-      width: 30,
-      height: 75,
-    });
 
     // if (localStorage.getItem("word-document1") === null) {
     var documentName = "empty";
@@ -198,35 +195,58 @@ export async function runPowerPoint() {
 
     Office.context.document.getFileAsync(Office.FileType.Pdf, { sliceSize: 4194304 }, (result) => {
       if (result.status === Office.AsyncResultStatus.Succeeded) {
+        localStorage.setItem("word-document-name1", documentName);
         const file = result.value;
         console.log(`slices: ${file.sliceCount}`);
-        file.getSliceAsync(0, (result) => {
-          if (result.status === Office.AsyncResultStatus.Succeeded) {
-            const { data } = result.value;
-            console.log("DATA: ");
-            console.log(data);
-            if (data) {
-              const buff = Buffer.from(data, "utf-8");
-              const base64 = buff.toString("base64");
-              console.log("base64: ");
-              console.log(base64);
-              localStorage.setItem("word-document1", base64);
-              localStorage.setItem("word-document-name1", documentName);
-              console.log("PowerPoint to PDF y guardado en LocalStorage:");
-              console.log(localStorage.getItem("word-document-name1"));
-              console.log(localStorage.getItem("word-document1"));
-              //console.log(Office.context.document.Name);
+        const totalSlices = file.sliceCount;
+        const doSomething = (currentSlice) =>
+          new Promise((resolve) => {
+            file.getSliceAsync(currentSlice, (result) => {
+              if (result.status === Office.AsyncResultStatus.Succeeded) {
+                const { data } = result.value;
+                const indice = result.value.index;
+                if (data) {
+                  const buff = Buffer.from(data, "utf-8");
+                  const base64 = buff.toString("base64");
+                  console.log(base64);
+                  localStorage.setItem(`word-document${indice + 1}`, base64);
+                  //console.log(`Word to PDF y guardado en sessionStorage ${indice + 1}`);
+                  resolve(currentSlice + 1 >= totalSlices ? "ok" : "no");
+                }
+              }
+            });
+            console.log("Here is " + currentSlice);
+          });
+
+        const loop = (currentSlice) =>
+          doSomething(currentSlice).then((result) => {
+            if (result === "ok") {
+              console.log("Finished slicing");
+              file.closeAsync((result) => {
+                console.log(result);
+                console.log(currentSlice);
+              });
+            } else {
+              console.log(currentSlice);
+              return loop(currentSlice + 1);
             }
-          }
-          file.closeAsync((result) => {
-            console.log(result.status);
+          });
+
+        loop(0).then(() => {
+          console.log("all done!");
+          document.getElementById("load-banner").style.display = "none";
+          Office.context.ui.displayDialogAsync("https://nervous-aryabhata-655da7.netlify.app/app.html", {
+            width: 30,
+            height: 75,
           });
         });
       } else {
+        document.getElementById("load-banner").style.display = "none";
+        document.getElementById("main-content").innerHTML =
+          "Error al transformar el documento. Recargue esta página e intente nuevamente";
         console.log("Error al cargar pdf ");
       }
     });
-    //}//
 
     await context.sync();
   });
